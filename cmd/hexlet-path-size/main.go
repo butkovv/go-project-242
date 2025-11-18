173431e22038da8b351f1c24d09529b4d4b48dd5
Вячеслав Бутько <butkovv1991@gmail.com>
2025-11-18 11:26:16 +0300

    Выполнен третий шаг

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -13,8 +13,13 @@ func main() {
 	cmd := &cli.Command{
 		Name:  "pathsize",
 		Usage: "print size of a file or directory",
-		Action: func(context.Context, *cli.Command) error {
-			fmt.Println("Hello, World!")
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			path := cmd.Args().Get(0)
+			size, err := GetSize(path)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%dB	%s\n", size, path)
 			return nil
 		},
 	}
@@ -22,4 +27,30 @@ func main() {
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		slog.Error(err.Error())
 	}
+}
+
+func GetSize(path string) (int, error) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return 0, err
+	}
+	size := 0
+	if info.IsDir() {
+		files, err := os.ReadDir(path)
+		if err != nil {
+			return 0, err
+		}
+		for _, f := range files {
+			if !f.IsDir() {
+				info, err := f.Info()
+				if err != nil {
+					return 0, err
+				}
+				size += int(info.Size())
+			}
+		}
+	} else {
+		size = int(info.Size())
+	}
+	return size, nil
 }
