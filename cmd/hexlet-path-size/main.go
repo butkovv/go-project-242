735dfc68248845383142158657d0baf038e8d10f
Вячеслав Бутько <butkovv1991@gmail.com>
2025-11-18 10:15:53 +0300

    Выполнен второй шаг

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	fmt.Println("Hello from Hexlet!")
+	cmd := &cli.Command{
+		Name:  "pathsize",
+		Usage: "print size of a file or directory",
+		Action: func(context.Context, *cli.Command) error {
+			fmt.Println("Hello, World!")
+			return nil
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		slog.Error(err.Error())
+	}
 }
